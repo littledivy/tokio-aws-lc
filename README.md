f620dbaa4aa0bd6636ebda8c25df381f03b7d63e
Divy Srivastava <dj.srivastava23@gmail.com>
2025-08-05 16:17:36 +0200

    Let's go

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ An implementation of SSL streams for Tokio built on top of the [`aws-lc-sys` cra
 
 Based on `tokio-openssl`
 
+```rust
+let ctx = aws_lc_sys::SSL_CTX_new(aws_lc_sys::TLS_method());
+let ssl = aws_lc_sys::SSL_new(ctx);
+
+let stream = tokio::net::TcpStream::connect("example.com:443").await?;
+let stream = tokio_aws_lc::SslStream::new(ssl, stream);
+
+stream.connect().await?;
+
+stream.write_all(b"GET / HTTP/1.0\r\nHost: example.com\r\n\r\n").await?;
+```
--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,6 @@
 use crate::SslStream;
-use futures_util::future;
 use aws_lc_sys as ffi;
+use futures_util::future;
 use std::net::ToSocketAddrs;
 use std::pin::Pin;
 use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
@@ -14,33 +14,33 @@ async fn google() {
     println!("TCP connection established");
 
     /*let ssl = SslConnector::builder(SslMethod::tls())
-        .unwrap()
-        .build()
-        .configure()
-        .unwrap()
-        .into_ssl("google.com")
-        .unwrap();*/
+    .unwrap()
+    .build()
+    .configure()
+    .unwrap()
+    .into_ssl("google.com")
+    .unwrap();*/
     let ssl = unsafe {
         let ctx = ffi::SSL_CTX_new(ffi::TLS_method());
         if ctx.is_null() {
             panic!("Failed to create SSL context");
         }
-        
+
         // Disable verification for testing
         ffi::SSL_CTX_set_verify(ctx, ffi::SSL_VERIFY_NONE, None);
-        
+
         // Set up cipher suites
         ffi::SSL_CTX_set_cipher_list(ctx, b"HIGH:!aNULL:!MD5\0".as_ptr() as *const _);
-        
+
         let ssl = ffi::SSL_new(ctx);
         if ssl.is_null() {
             panic!("Failed to create SSL object");
         }
-        
+
         // Set hostname for SNI
         ffi::SSL_set_tlsext_host_name(ssl, b"httpbin.org\0".as_ptr() as *const _);
         ffi::SSL_set_connect_state(ssl);
-        
+
         // Free the context as it's not needed after SSL object creation
         ffi::SSL_CTX_free(ctx);
         ssl
@@ -58,15 +58,18 @@ async fn google() {
                 if err != 0 {
                     let mut buf = [0u8; 256];
                     aws_lc_sys::ERR_error_string_n(err, buf.as_mut_ptr() as *mut i8, buf.len());
-                    let error_str = std::ffi::CStr::from_ptr(buf.as_ptr() as *const i8).to_string_lossy();
+                    let error_str =
+                        std::ffi::CStr::from_ptr(buf.as_ptr() as *const i8).to_string_lossy();
                     println!("SSL error string: {}", error_str);
                 }
-                
+
                 // Print all errors in the error queue
                 let mut all_errors = Vec::new();
                 loop {
                     let err = aws_lc_sys::ERR_get_error();
-                    if err == 0 { break; }
+                    if err == 0 {
+                        break;
+                    }
                     all_errors.push(err);
                 }
                 println!("All SSL errors: {:?}", all_errors);
@@ -86,4 +89,3 @@ async fn google() {
     assert!(response.starts_with("HTTP/1.0 "));
     assert!(response.ends_with("</html>") || response.ends_with("</HTML>"));
 }
-
